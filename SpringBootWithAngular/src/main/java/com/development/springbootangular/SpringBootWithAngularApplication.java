c634f72c5f5c41118332b965d2df5953297d1fcc
Luiz Mandico <luizmandico@MacBook-Air-de-Luiz.local>
2022-01-31 17:38:37 -0300

    Green

--- SpringBootWithAngular/src/main/java/com/development/springbootangular/SpringBootWithAngularApplication.java
+++ SpringBootWithAngular/src/main/java/com/development/springbootangular/SpringBootWithAngularApplication.java
@@ -3,7 +3,6 @@ package com.development.springbootangular;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class SpringBootWithAngularApplication {
 
